13d81dfd104613c47425ee3d8f3e8314d1e72607
0712023 <0712023@hanmail.net>
2020-12-15 18:46:47 +0900

    ver 1.1

--- replace_ver1.1.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def replace():
-    os.system('sudo rm -v /home/pi/workspace/test.conf')
-    os.system('sudo touch /home/pi/workspace/new.conf')
-    with open("/home/pi/workspace/new.conf", "a") as wpa:
-        wpa.write("ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev\nupdate_config=1\ncountry=US\n\nnetwork={\n	ssid=\"skku\"\n	key_mgmt=NONE\n}")
-
-replace()
