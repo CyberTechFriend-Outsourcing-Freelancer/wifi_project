1fb5a42d98fb1c3f3736b4ffc562c56385d4967e
0712023 <0712023@hanmail.net>
2020-12-15 18:47:13 +0900

    ver 1.3

--- replace.py
+++ replace.py
@@ -1,12 +1,10 @@
 import os
 
 def replace(SSID, PASSWORD):
-    os.system('sudo rm -v /home/pi/workspace/old.conf')
-    os.system('sudo touch /home/pi/workspace/new.conf')
-    with open("/home/pi/workspace/new.conf", "a") as wpa:
+    os.system('sudo rm -v /etc/wpa_supplicant/wpa_supplicant.conf')
+    os.system('sudo touch /etc/wpa_supplicant/wpa_supplicant.conf')
+    with open("/etc/wpa_supplicant/wpa_supplicant.conf", "a") as wpa:
         wpa.write("ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev\nupdate_config=1\ncountry=US\n\nnetwork={\n	ssid=\""+SSID+"\"\n	psk=\""+PASSWORD+"\"\n	key_mgmt=WPA-PSK\n}")
+    os.system('sudo systemctl restart dhcpcd')
 
 replace(input('SSID : '),input('Password : '))
-
-#SSID : 0712023   PASSWORD : omin6743
-#SSID : 0712023_5G   PASSWORD : omin6743
