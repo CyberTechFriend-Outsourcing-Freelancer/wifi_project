4eddc87448a39ed7b08b3f04224df71c209cf059
0712023 <0712023@hanmail.net>
2020-12-15 18:47:01 +0900

    ver 1.2

--- replace.py
+++ replace.py
@@ -1,9 +1,12 @@
 import os
 
-def replace():
-    os.system('sudo rm -v /home/pi/workspace/test.conf')
+def replace(SSID, PASSWORD):
+    os.system('sudo rm -v /home/pi/workspace/old.conf')
     os.system('sudo touch /home/pi/workspace/new.conf')
     with open("/home/pi/workspace/new.conf", "a") as wpa:
-        wpa.write("ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev\nupdate_config=1\ncountry=US\n\nnetwork={\n	ssid=\"skku\"\n	key_mgmt=NONE\n}")
+        wpa.write("ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev\nupdate_config=1\ncountry=US\n\nnetwork={\n	ssid=\""+SSID+"\"\n	psk=\""+PASSWORD+"\"\n	key_mgmt=WPA-PSK\n}")
 
-replace()
+replace(input('SSID : '),input('Password : '))
+
+#SSID : 0712023   PASSWORD : omin6743
+#SSID : 0712023_5G   PASSWORD : omin6743
